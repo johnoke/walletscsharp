10e88198bcd7a24106b52203a736c6d01a0dbbfa
victor bolum <victor@wallet.ng>
2021-05-25 15:01:51 +0100

    Add Wallet Service

--- WalletsAfrica/Constants/Endpoints.cs
+++ WalletsAfrica/Constants/Endpoints.cs
@@ -6,5 +6,17 @@ namespace WalletsAfrica.Constants
         public const string BALANCE = "/self/balance";
         public const string TRANSACTIONS = "/self/transactions";
 
+
+        public const string DEBITWALLET = "/wallet/debit";
+        public const string CREDITWALLET = "/wallet/credit";
+        public const string GENERATESUBWALLET = "/wallet/generate";
+        public const string RETRIEVEACCOUNTNUMBER = "/wallet/nuban";
+        public const string SETPASSWORD = "/wallet/password";
+        public const string SETPIN = "/wallet/pin";
+        public const string WALLETTRANSACTIONS = "/wallet/transactions";
+        public const string VERIFYBVN = "/wallet/verifybvn";
+        public const string GETUSER = "/wallet/getuser";
+        public const string WALLETBALANCE = "/wallet/balance";
+
     }
 }
---
+++ WalletsAfrica/Entities/Wallet/Debit.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using WalletsAfrica.Entities.Useful;
+
+namespace WalletsAfrica.Entities.Wallet
+{
+    public class DebitResponse
+    {
+        public GenericResponse Response { get; set; }
+        public DebitResponseData Data { get; set; }       
+
+
+    }
+    public class DebitResponseData
+    {
+        public decimal AmountCredited { get; set; }
+        public decimal CustomerWalletBalance { get; set; }
+        public decimal DeveloperWalletBalance { get; set; }
+    }
+
+}
